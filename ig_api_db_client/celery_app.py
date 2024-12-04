b1045421a3355a2fab1e09a2b735a6637883b571
KidnDaCandyStore <sonic40191@gmail.com>
2024-12-03 19:59:45 -0500

    Update celery_app.py

--- ig_api_db_client/celery_app.py
+++ ig_api_db_client/celery_app.py
@@ -5,11 +5,16 @@ from .config import Config
 def make_celery(app):
     celery = Celery(
         app.import_name,
-        broker=Config.CELERY_BROKER_URL,
-        backend=Config.CELERY_RESULT_BACKEND,
+        broker=app.config['broker_url'],
+        backend=app.config['result_backend'],
         include=['ig_api_db_client.tasks']
     )
-    celery.conf.update(app.config)
+    celery.conf.update(
+        broker_url=app.config['broker_url'],
+        result_backend=app.config['result_backend'],
+        worker_log_level='INFO',  # Set the desired logging level
+        # Include other necessary Celery configurations
+    )
 
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
