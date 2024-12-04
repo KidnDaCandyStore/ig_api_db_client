2e80e08e778ef7155053dec100371c8dd060f786
KidnDaCandyStore <sonic40191@gmail.com>
2024-12-03 20:04:43 -0500

    Update config.py

--- ig_api_db_client/config.py
+++ ig_api_db_client/config.py
@@ -7,7 +7,13 @@ class Config:
     INSTAGRAM_USERNAME = os.getenv('INSTAGRAM_USERNAME')
     INSTAGRAM_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
     SECRET_KEY = os.getenv('SECRET_KEY')
-    broker_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
-    result_backend = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+    REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+
+    # Ensure REDIS_URL includes database number
+    if REDIS_URL.count('/') == 2:
+        REDIS_URL += '/0'
+
+    CELERY_BROKER_URL = REDIS_URL
+    CELERY_RESULT_BACKEND = REDIS_URL
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
